b44fdcf70e70e6f433ba5924114a758ce798aa3b
slim <slimabida21@gmail.com>
2025-05-20 11:10:34 +0200

    mise en place d'une onction java pour faire dispariatre les messages addFlash après 3 seconde et mise en place du formulaire de connexion style

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,18 @@
 // JS 
+// Affiche les messages flash pendant 3 secondes avant de les faire disparaître//
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const flashMessages = document.querySelectorAll('.flash-message');
+
+        flashMessages.forEach(function (message) {
+            setTimeout(function () {
+                message.style.transition = "opacity 0.5s ease-out";
+                message.style.opacity = '0';
+
+                // Supprime l'élément après le fondu
+                setTimeout(function () {
+                    message.remove();
+                }, 500);
+            }, 3000); // Disparaît après 3 secondes
+        });
+    });
