6cdefb5e44bde52ff063f84053652621a402d26a
slim <slimabida21@gmail.com>
2025-05-20 10:51:16 +0200

    stylisation du formulaire d'inscription et de connexion test inscription OK BDD

--- src/Form/RegistrationForm.php
+++ src/Form/RegistrationForm.php
@@ -6,6 +6,8 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,31 +19,66 @@ class RegistrationForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class,[
+                'label' => 'Email<span class="text-gold">*</span>',
+                'label_html' => true,
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Saisir votre email',
+                    
+                ],
+                
+                ])
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'J\'accepte les conditions d\'utilisation',
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous acceptez nos conditions d\'utilisation.',
                     ]),
                 ],
             ])
-            ->add('firstName')
-            ->add('lastName')
+            ->add('firstName', textType::class,[
+                'label' => 'Prénom<span class="text-gold">*</span>',
+                'label_html' => true,
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Saisir votre prénom',
+                    
+                ],
+                
+                ])
+            ->add('lastName',textType::class,[
+                'label' => 'Nom<span class="text-gold">*</span>',
+                'label_html' => true,
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Saisir votre nom',
+                    
+                ],
+                
+                ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'required' => false,
+                'label' => 'Mot de passe<span class="text-gold">*</span>',
+                'label_html' => true,
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'placeholder' => 'Saisir votre mot de passe',
+                
+                ],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Entrez votre mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
-                        'max' => 4096,
+                        'max' => 12,
                     ]),
                 ],
             ])
